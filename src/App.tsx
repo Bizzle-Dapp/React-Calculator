7a4929abafe8cd5001def163cbe2d59d7540cd31
Bizzle Dapp <36421291+Bizzle-Dapp@users.noreply.github.com>
2020-01-22 13:49:29 +0000

    Updated the funky UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { CalculatorBody, Button, Input } from './styled-constants';
 function App() {
   const [calcResult, setCalcResult] = useState(0);
   const [selectedCalcFunction, setSelectedCalcFunction] = useState('None');
-  const [numericInput, setNumericInput] = useState(0);
+  const [numericInput1, setNumericInput1] = useState(0);
+  const [numericInput2, setNumericInput2] = useState(0);
 
   const updateSelectedCalcFunction = (val: string) => {
     if(val === selectedCalcFunction)
@@ -36,16 +37,16 @@ function App() {
     switch(selectedCalcFunction)
     {
       case "+":
-        setCalcResult(startingValue + numericInput);
+        setCalcResult(numericInput1 + numericInput2);
         return;
       case "-":
-        setCalcResult(startingValue - numericInput);
+        setCalcResult(numericInput1 - numericInput2);
         return;
       case "÷":
-        setCalcResult(startingValue / numericInput);
+        setCalcResult(numericInput1 / numericInput2);
         return;
       case "x":
-        setCalcResult(startingValue * numericInput);
+        setCalcResult(numericInput1 * numericInput2);
         return;
     }
 
@@ -59,6 +60,10 @@ function App() {
         <div style={{background: "#c5c5c5", height: "50px", borderTopLeftRadius: "inherit", borderTopRightRadius: "inherit", textAlign:"right", padding:"5px 10px 0px 10px"}}> {/* Calculator Result Display */}
           {calcResult}
         </div>
+        <div> {/* Numeric Input1 */}
+          <Input type="number" value={numericInput1} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setNumericInput1(Number(e.target.value))}}></Input>
+          <Button onClick={()=> {setNumericInput1(numericInput1 * -1)}}>+/-</Button>
+        </div>
         <div> {/* Calculator Function Buttons */}
           <Button style={{background: "rgb(197, 197, 197)", marginRight:"15px"}}>Selected: {selectedCalcFunction}</Button>
           <Button onClick={() => {updateSelectedCalcFunction("+")}}>+</Button>
@@ -66,13 +71,13 @@ function App() {
           <Button onClick={() => {updateSelectedCalcFunction("÷")}}>÷</Button>
           <Button onClick={() => {updateSelectedCalcFunction("x")}}>x</Button>
         </div>
-        <div> {/* Numeric Input */}
-          <Input type="number" value={numericInput} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setNumericInput(Number(e.target.value))}}></Input>
-          <Button onClick={()=> {setNumericInput(numericInput * -1)}}>+/-</Button>
+        <div> {/* Numeric Input2 */}
+          <Input type="number" value={numericInput2} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setNumericInput2(Number(e.target.value))}}></Input>
+          <Button onClick={()=> {setNumericInput2(numericInput2 * -1)}}>+/-</Button>
         </div>
         <div>{/* Go and Clear Buttons */}
-          <Button onClick={() => {performCalculation()}}>=</Button>
-          <Button onClick={() => {setCalcResult(0)}}>Clear</Button>
+          <Button onClick={() => {performCalculation(); setNumericInput1(0); setNumericInput2(0)}}>=</Button>
+          <Button onClick={() => {setCalcResult(0); setNumericInput1(0); setNumericInput2(0)}}>Clear</Button>
         </div>
       </CalculatorBody>
     </div>
