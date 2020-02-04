ff05806898c5d7336f1ecd9bbbe9c9dd1dfe9ae2
Bizzle Dapp <36421291+Bizzle-Dapp@users.noreply.github.com>
2020-02-04 23:03:02 +0000

    Trying to thin out the CSS so this works as a WordPress embed. The problem is not my CSS, it's Wordpress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,79 @@
 import React , { useState } from 'react';
-import './App.css';
 import  Logo from './widgets';
-import { CalculatorBody, ResultView, Button, Input } from './styled-constants';
+import styled from 'styled-components';
 
 function App() {
+  const Body = styled.div`
+    margin: 15px !important;
+    text-align: center !important;
+    min-height: 50vh !important;
+    display: block !important;
+    align-items: center !important;
+    justify-content: center !important;
+    font-size: calc(10px + 2vmin) !important;
+    color: white !important;
+    margin: auto !important;
+    width: 95% !important;
+
+  @media (prefers-reduced-motion: no-preference) {
+    .Logo {
+      margin-top: 25px;
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  `;
+
+ const CalculatorBody = styled.div`
+    margin: auto !important;
+    width: 100% !important;
+    border-radius: 10px !important;
+    border: 2px solid #a0a0a0 !important;
+`;
+
+ const ResultView = styled.div`
+    height: 50px !important; 
+    border-bottom: 2px white solid !important;
+    border-top-left-radius: inherit !important; 
+    border-top-right-radius: inherit !important;
+    border-bottom-right-radius: 0px !important;
+    border-bottom-left-radius: 0px !important;
+    text-align: right !important; 
+    padding: 5px 10px 5px 10px !important;
+`;
+
+ const Button = styled.button`
+    color: white !important; 
+    margin: 2px !important;
+    padding: 5px 10px 5px 10px !important; 
+    background: #8a2be2 !important; 
+    border-radius: 5px !important; 
+    box-shadow: -1px 1px 2px 1px rgb(115, 109, 109) !important;
+    font-size: medium !important;
+    font-family: sans-serif !important;
+    font-weight: 600 !important;
+`;
+
+ const Input = styled.input`
+    color: black !important;
+    width: 25% !important; 
+    margin: 2px !important;
+    padding: 5px 10px 5px 10px !important; 
+    background: rgb(197, 197, 197) !important;
+    border-radius: 5px !important; 
+    box-shadow: -1px 1px 2px 1px rgb(115, 109, 109) !important;
+    font-size: medium !important;
+    font-family: sans-serif !important;
+    font-weight: 600 !important;
+`;
+
   const [calcResult, setCalcResult] = useState(0);
   const [selectedCalcFunction, setSelectedCalcFunction] = useState('None');
   const [numericInput1, setNumericInput1] = useState(0);
@@ -53,12 +123,12 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <Body>
       <Logo/>
       <p>Calculator</p>
       <CalculatorBody> {/* Calculator Body */}
         <ResultView > {/* Calculator Result Display */}
-          <div style={{background: "white", padding: "2px 5px 2px 5px", borderRadius:"5px", color:"black"}}>
+          <div style={{background: "white", padding: "2px 5px 2px 5px", borderTopLeftRadius:"5px", borderTopRightRadius:"5px", color:"black"}}>
             {calcResult}
           </div>
         </ResultView>
@@ -84,7 +154,7 @@ function App() {
           <Button onClick={() => {setCalcResult(0); setNumericInput1(0); setNumericInput2(0); setContinuedCalculation(false);}}>Clear</Button>
         </div>
       </CalculatorBody>
-    </div>
+    </Body>
   );
 }
 
